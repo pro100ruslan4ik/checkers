a758d76d4dc89397836cbe8992a28802c036f109
pro100rusl <haertdinov.ruslan@gmail.com>
2024-06-13 07:48:13 +0500

    Сделать документацию, добавить сообщение о победе, переименовать методы

--- MainWindowForm.Designer.cs
+++ MainWindowForm.Designer.cs
@@ -36,9 +36,10 @@
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
             Icon = (Icon)resources.GetObject("$this.Icon");
+            MaximizeBox = false;
             Name = "MainWindowForm";
-            Text = "MenuForm";
             ResumeLayout(false);
         }
 
